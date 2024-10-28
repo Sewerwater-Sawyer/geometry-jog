2d8840ad37eab1b178df06b366a646e69f7307c9
Sawyer Deegan <Legomaster2334@gmail.com>
2024-10-27 20:13:27 -0600

    Size constraints

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-import './style.css'
+//@ts-check
+import './css/style.css'
+import Phaser from 'phaser';
+
+/** @type { Phaser.Types.Core.GameConfig } */
+const config = {
+    width: 600,
+    height: 300,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH, 
+    },
+    parent: "app",
+};
+
+const game = new Phaser.Game(config);
