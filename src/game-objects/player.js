8c8092d0d3ba4c1241ee9a1098b1ccd144825544
Sawyer Deegan <Legomaster2334@gmail.com>
2024-11-04 16:04:18 -0700

    Adjusting teens

--- src/game-objects/player.js
+++ src/game-objects/player.js
@@ -21,14 +21,23 @@ export class Player extends Phaser.GameObjects.Rectangle {
             Phaser.Input.Keyboard.KeyCodes.SPACE
         );
 
-        this.jumpTween = undefined;
+
+
+        this.jumpTween = this.scene.tweens.add ({
+            targets: this,
+            duration: 1000,
+            repeat: -1,
+            angle: { from: 0, to: 360 }
+        });
     }
+}
+
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.space)) {
             this.jump();
         } else if (this.body.blocked.down) {
-            this.jumpTween?.stop();
+            this.jumpTween?.restart();
             this.rotation = 0;
         }
     }
@@ -39,12 +48,4 @@ export class Player extends Phaser.GameObjects.Rectangle {
         }
 
         this.body.setVelocityY (-350);
-
-        this.jumpTween = this.scene.tweens.add ({
-            targets: this,
-            duration: 1000,
-            repeat: -1,
-            angle: { from: 0, to: 360 }
-        });
-    }
 }
